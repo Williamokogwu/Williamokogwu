083c9eb2bedf683f36275ab3792a324130a23538
Chijioke Okogwu <161358292+Williamokogwu@users.noreply.github.com>
2026-07-22 00:13:32 -0500

    Remove base URL from Vite config
    Remove base URL configuration for deployment.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,4 @@ import react from '@vitejs/plugin-react'
 // Enables Vite's React transform and development integration for JSX files.
 export default defineConfig({
   plugins: [react()],
-
-  base: 'williamokogwu.github.io',
 })
